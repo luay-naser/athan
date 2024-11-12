74977515cbc57fc00357bf667de3926e160861f1
luay-naser <162501192+luay-naser@users.noreply.github.com>
2024-11-13 00:09:36 +0300

    Add files via upload

---
+++ athan.js
@@ -0,0 +1,153 @@
+// function times(){
+//   let times = new Date();
+//    let ti = times.getDate() + "/" + (times.getMonth() + 1) + "/" + times.getFullYear() + " | " + times.getHours() + ":" + times.getMinutes();
+// return ti;
+//   }
+//   document.getElementById("day").innerHTML = times()
+function time(){
+  axios.get(`http://api.aladhan.com/v1/timingsByCity?city=اسطنبول&country=tr&month=11&year=2024`)
+.then((response) =>{
+  let data = response.data.data.timings;
+  let content = `
+          <div class="card">
+            <div class="title">
+                <h4>الفجر</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Fajr}</h5>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="title">
+                <h4>الشروق</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Sunrise}</h5>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="title">
+                <h4>الظهر</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Dhuhr}</h5>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="title">
+                <h4>العصر</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Asr}</h5>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="title">
+                <h4>المغرب</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Maghrib}</h5>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="title">
+                <h4>العشاء</h4>
+            </div>
+            <div class="body">
+                <h5>${data.Isha}</h5>
+            </div>
+
+        </div>
+  `
+  let box = document.getElementById("cards")
+  box.innerHTML = content;
+let dayName = response.data.data.date.hijri;
+document.getElementById("day").innerHTML = dayName.weekday.ar;
+let dayNum = response.data.data.date.gregorian;
+document.getElementById("day").innerHTML +=  " " +dayNum.day + " " + dayNum.month.en;
+})
+.catch((error)=>{
+  console.log("its", error)
+})
+
+
+}
+
+time()
+
+document.getElementById("head").innerHTML = "اسطنبول";
+function city(){
+  let cityname = document.getElementById("cityName").value
+  document.getElementById("head").innerHTML = cityname;
+
+  axios.get(`http://api.aladhan.com/v1/timingsByCity?city=${cityname}&country=tr&month=11&year=2024`)
+  .then((response) =>{
+    let data = response.data.data.timings;
+    let content = `
+            <div class="card">
+              <div class="title">
+                  <h4>الفجر</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Fajr}</h5>
+              </div>
+  
+          </div>
+          <div class="card">
+              <div class="title">
+                  <h4>الشروق</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Sunrise}</h5>
+              </div>
+  
+          </div>
+          <div class="card">
+              <div class="title">
+                  <h4>الظهر</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Dhuhr}</h5>
+              </div>
+  
+          </div>
+          <div class="card">
+              <div class="title">
+                  <h4>العصر</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Asr}</h5>
+              </div>
+  
+          </div>
+          <div class="card">
+              <div class="title">
+                  <h4>المغرب</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Maghrib}</h5>
+              </div>
+  
+          </div>
+          <div class="card">
+              <div class="title">
+                  <h4>العشاء</h4>
+              </div>
+              <div class="body">
+                  <h5>${data.Isha}</h5>
+              </div>
+  
+          </div>
+    `
+    let box = document.getElementById("cards")
+    box.innerHTML = content;
+  })
+  .catch((error)=>{
+    console.log("its", error)
+  })
+}
